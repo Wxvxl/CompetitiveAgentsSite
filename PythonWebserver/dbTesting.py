6e432896b6f755421be7628853872ba9860200fc
Wxvxl <metal123cool1115@gmail.com>
2025-08-28 02:02:03 +0800

    Script that will generate a test database

--- PythonWebserver/dbTesting.py
+++ PythonWebserver/dbTesting.py
@@ -1,12 +1,67 @@
 import psycopg2
 
+def insert_row(cursor, groupname, code):
+    cursor.execute(f"""
+        INSERT INTO scripts (groupname, code)
+        VALUES (%s, %s)
+    """, (groupname, code))
+
+
+# Replace this with your postgres password during setup.
+user_name = "postgres"
+password = "admin"
+
 conn = psycopg2.connect(
-    dbname="testdb", user="postgres", password="admin", host="localhost"
+    dbname="postgres",
+    user=user_name,
+    password=password,
+    host="localhost",
+    port="5432"
 )
 
+conn.autocommit = True
 cur = conn.cursor()
 
-cur.execute("SELECT code FROM scripts WHERE name = %s", ("randomagent",))
-result = cur.fetchone()
+db_name = "testing_database"
 
-print(result[0])
+cur.execute("SELECT 1 FROM pg_database WHERE datname = %s", (db_name,))
+
+if cur.fetchone():
+    cur.execute(cur.execute(f"DROP DATABASE {db_name}"))
+    
+cur.execute(f"CREATE DATABASE {db_name}")
+cur.execute("""CREATE TABLE IF NOT EXISTS scripts (
+                groupname VARCHAR(100) PRIMARY KEY,
+                code TEXT
+            );
+        """)
+
+insert_row(cur, "g1", """from PythonWebserver.baseclass import Agents
+import random
+
+class RandomAgents(Agents):
+    choices = ["r", "p", "s"]
+    def __init__(self):
+        super().__init__()
+    
+    def getAction(self):
+        return self.choices[random.randint(0,2)]
+""")
+
+
+insert_row(cur, "g2", """from PythonWebserver.baseclass import Agents
+import random
+
+class RandomAgents(Agents):
+    choices = ["r", "p", "s"]
+    def __init__(self):
+        super().__init__()
+    
+    def getAction(self):
+        return self.choices[random.randint(0,2)]
+""")
+
+cur.close()
+conn.close()
+
+print("Success")
